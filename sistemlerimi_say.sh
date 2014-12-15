2fe54f2a1684137f0d48baf72c94e44396618124
omulinux <omulinux.groups@gmail.com>
2014-12-15 22:06:35 +0200

    Create sistemlerimi_say.sh

---
+++ sistemlerimi_say.sh
@@ -0,0 +1,8 @@
+echo "Sistemdeki parola olan kullanici sayisi :\c"
+cat /etc/passwd | wc -l
+
+echo "Sistemde kac tane kullanici var : \c"
+who | wc -l
+
+echo "Calisan proseslerinin toplami : \c"
+ps -e | wc -l
