3387640c8d2f2b0769a73bb13a6524e33121f7a1
omulinux <omulinux.groups@gmail.com>
2014-12-15 22:13:55 +0200

    Update sistemlerimi_say.sh

--- sistemlerimi_say.sh
+++ sistemlerimi_say.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 echo "Sistemdeki parola olan kullanici sayisi :\c"
 cat /etc/passwd | wc -l
 
-echo "Sistemde kac tane kullanici var : \c"
+echo "Sistemde kac tane kullanici var :\c"
 who | wc -l
 
-echo "Calisan proseslerinin toplami : \c"
+echo "Calisan proseslerinin toplami :\c"
 ps -e | wc -l
